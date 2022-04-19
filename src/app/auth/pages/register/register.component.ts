59725eece59cfb2b34ed5cce29b6effd8e488ec6
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-18 23:26:01 -0300

    register

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -13,9 +15,20 @@ export class RegisterComponent {
     email: ['test4@test.com', [Validators.required, Validators.email]],
     password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   login() {
-    this.router.navigateByUrl('/dashboard');
+    const { name, email, password } = this.miFormulario.value;
+    this.authService.registro(name, email, password).subscribe((ok) => {
+      if (ok === true) {
+        this.router.navigateByUrl('/dashboard');
+      } else {
+        Swal.fire('Error!', ok, 'error');
+      }
+    });
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -17,6 +17,22 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
+  registro(name: string, email: string, password: string) {
+    const url = `${this.baseUrl}/auth/new`;
+    const body = { name, email, password };
+    return this.http.post<AuthResponse>(url, body).pipe(
+      tap((resp) => {
+        localStorage.setItem('token', resp.token!);
+        this._usuario = {
+          name: resp.name!,
+          uid: resp.uid!,
+        };
+      }),
+      map((resp) => resp.ok),
+      catchError((err) => of(err.error.msg))
+    );
+  }
+
   login(email: string, password: string) {
     const url = `${this.baseUrl}/auth`;
     const body = { email, password };
@@ -53,5 +69,9 @@ export class AuthService {
       }),
       catchError((err) => of(false))
     );
+  }
+
+  logout() {
+    localStorage.clear();
   }
 }
