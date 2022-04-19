d470c62a7ac30a6b1b7804052f8010788844fb44
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-18 23:05:05 -0300

    guard validate token

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, map, of, tap } from 'rxjs';
+import { catchError, map, of, tap, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { AuthResponse, Usuario } from '../interfaces/interfaces';
 
@@ -20,10 +20,10 @@ export class AuthService {
   login(email: string, password: string) {
     const url = `${this.baseUrl}/auth`;
     const body = { email, password };
-
     return this.http.post<AuthResponse>(url, body).pipe(
       tap((resp) => {
         if (resp.ok) {
+          localStorage.setItem('token', resp.token!);
           this._usuario = {
             name: resp.name!,
             uid: resp.uid!,
@@ -32,6 +32,26 @@ export class AuthService {
       }),
       map((resp) => resp.ok),
       catchError((err) => of(err.error.msg))
+    );
+  }
+
+  validarToken(): Observable<boolean> {
+    const url = `${this.baseUrl}/auth/renew`;
+    const headers = new HttpHeaders().set(
+      'x-token',
+      localStorage.getItem('token') || ''
+    );
+
+    return this.http.get<AuthResponse>(url, { headers }).pipe(
+      map((resp) => {
+        localStorage.setItem('token', resp.token!);
+        this._usuario = {
+          name: resp.name!,
+          uid: resp.uid!,
+        };
+        return resp.ok;
+      }),
+      catchError((err) => of(false))
     );
   }
 }
