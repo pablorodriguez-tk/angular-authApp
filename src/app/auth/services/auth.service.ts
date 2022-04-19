c3904277903444db93c0dba8fd002a771ddd6357
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-18 23:45:25 -0300

    add email on user

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -21,12 +21,10 @@ export class AuthService {
     const url = `${this.baseUrl}/auth/new`;
     const body = { name, email, password };
     return this.http.post<AuthResponse>(url, body).pipe(
-      tap((resp) => {
-        localStorage.setItem('token', resp.token!);
-        this._usuario = {
-          name: resp.name!,
-          uid: resp.uid!,
-        };
+      tap(({ ok, token }) => {
+        if (ok) {
+          localStorage.setItem('token', token!);
+        }
       }),
       map((resp) => resp.ok),
       catchError((err) => of(err.error.msg))
@@ -40,10 +38,6 @@ export class AuthService {
       tap((resp) => {
         if (resp.ok) {
           localStorage.setItem('token', resp.token!);
-          this._usuario = {
-            name: resp.name!,
-            uid: resp.uid!,
-          };
         }
       }),
       map((resp) => resp.ok),
@@ -64,6 +58,7 @@ export class AuthService {
         this._usuario = {
           name: resp.name!,
           uid: resp.uid!,
+          email: resp.email!,
         };
         return resp.ok;
       }),
